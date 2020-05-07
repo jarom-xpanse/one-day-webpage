8575c301406be6595038c2e54bf5f52d6df7bd05
jarom-xpanse <jarom.xpanse@gmail.com>
2020-05-07 14:38:51 +0800

    korsi header now started

--- korsi/tailwind.config.js
+++ korsi/tailwind.config.js
@@ -1,8 +1,27 @@
 module.exports = {
-  purge: [],
-  theme: {
-    extend: {},
+  // ...
+  corePlugins: {
+    container: false
   },
-  variants: {},
-  plugins: [],
+  plugins: [
+    function ({ addComponents }) {
+      addComponents({
+        '.container': {
+          maxWidth: '90%',
+          '@screen sm': { //640
+            // maxWidth: '600px',
+          },
+          '@screen md': { //768
+            // maxWidth: '728px',
+          },
+          '@screen lg': { //1024
+            maxWidth: '984px',
+          },
+          '@screen xl': { //1280
+            maxWidth: '1240px',
+          },
+        }
+      })
+    }
+  ]
 }
